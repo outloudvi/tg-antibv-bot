25f67b8f599df59d689f8fd86e2e7fbbe40b0c4b
Outvi V <git@outv.im>
2025-01-01 09:33:54 +0800

    tests: remove expired tests

--- tests/test.ts
+++ tests/test.ts
@@ -32,67 +32,6 @@ describe('Library test suite', function () {
     expect(bv2av('BV1L9Uoa9EUx')).to.eq('111298867365120')
   })
 
-  it('getResp (b23->BV->av)', async function () {
-    await checkUrl(
-      'b23.tv/eFXEF1',
-      'https://www.bilibili.com/video/av328843878',
-      ['BV1CA411e7eA']
-    )
-  })
-
-  it('getResp (b23->cv)', async function () {
-    await checkUrl('b23.tv/osjFE5', 'https://www.bilibili.com/read/cv7603961')
-  })
-
-  it('getResp (b23->others)', async function () {
-    await checkUrl(
-      'b23.tv/7fZElg',
-      'https://bml.bilibili.com/2020/index.html#/vr'
-    )
-  })
-
-  it('getResp (multiple)', async function () {
-    const resp = await getResp('https://b23.tv/eFXEF1  b23.tv/osjFE5')
-    expect(resp).to.deep.eq([
-      buildGoodResult(
-        'b23.tv/eFXEF1',
-        'https://www.bilibili.com/video/av328843878',
-        ['BV1CA411e7eA']
-      ),
-      buildGoodResult(
-        'b23.tv/osjFE5',
-        'https://www.bilibili.com/read/cv7603961',
-        []
-      ),
-    ])
-  })
-
-  it('special tests', async function () {
-    // issue #1
-    await checkUrl('BV1aV411z7gR', 'https://www.bilibili.com/video/av414110286')
-
-    await checkUrl(
-      'b23.tv/kGHdWU',
-      'https://www.bilibili.com/video/av805474857',
-      ['BV1934y1Q7HF']
-    )
-  })
-
-  it('IAP link', async function () {
-    await checkUrl(
-      'b23.tv/mXtAjY',
-      'https://www.bilibili.com/video/av846101025'
-    )
-  })
-
-  // https://github.com/nicholascw/b23.wtf/issues/2
-  it('b23.wtf#2', async function () {
-    await checkUrl(
-      'b23.tv/0xiavP',
-      'https://mall.bilibili.com/detail.html?itemsId=10041024'
-    )
-  })
-
   it('#7', async function () {
     await checkUrl(
       'bili2233.cn/BmOPBLD',
